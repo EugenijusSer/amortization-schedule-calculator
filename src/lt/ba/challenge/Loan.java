c34076af2e9e336b012b2697e773e5caf377bf33
EugenijusSer <eugenijus.sersniovas@vka.lt>
2018-05-26 15:27:37 +0300

    Fill Loan model

--- src/lt/ba/challenge/Loan.java
+++ src/lt/ba/challenge/Loan.java
@@ -1,4 +1,34 @@
 package lt.ba.challenge;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Loan {
+    private Date startDate;
+    private BigDecimal presentValue;
+    private BigDecimal interestRate;
+    private int numberOfPeriods;
+
+    Loan(Date startDate, BigDecimal presentValue, BigDecimal interestRate, int numberOfPeriods) {
+        this.startDate = startDate;
+        this.presentValue = presentValue;
+        this.interestRate = interestRate.movePointLeft(2);
+        this.numberOfPeriods = numberOfPeriods;
+    }
+
+    public Date getStartDate() { return startDate; }
+
+    public void setStartDateDate(Date startDate) { this.startDate = startDate; }
+
+    public BigDecimal getPresentValue() { return presentValue; }
+
+    public void setPresentValue(BigDecimal presentValue) { this.presentValue = presentValue; }
+
+    public BigDecimal getInterestRate() { return interestRate; }
+
+    public void setInterestRate(BigDecimal interestRate) { this.interestRate = interestRate; }
+
+    public int getNumberOfPeriods() { return numberOfPeriods; }
+
+    public void setNumberOfPeriods(int numberOfPeriods) { this.numberOfPeriods = numberOfPeriods; }
 }
